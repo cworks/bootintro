e532a2b807ca31974abeacc47d689ea564d00113
cworks <cworkscode@gmail.com>
2015-12-24 11:15:24 -0600

    inital project files

---
+++ src/main/java/cworks/bootintro/config/WebConfig.java
@@ -0,0 +1,14 @@
+package cworks.bootintro.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Creates
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+
+}
